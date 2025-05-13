21b6a5d39c3a9d05e2bd16ffd2c0e0dc928c15b4
mosaro404@gmail.com <mosaro404@gmail.com>
2025-05-13 23:02:22 +0200

    new pic

--- assets/assets.js
+++ assets/assets.js
@@ -39,8 +39,10 @@ import right_arrow from './right-arrow.png';
 import send_icon from './send-icon.png';
 import right_arrow_bold from './right-arrow-bold.png';
 import right_arrow_bold_dark from './right-arrow-bold-dark.png';
+import about_pic from './about_pic.jpg';
 
 export const assets = {
+    about_pic,
     phone_icon,
     user_image,
     mypic,
