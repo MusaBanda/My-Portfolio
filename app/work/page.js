419e86036daf1951ec3ba42268eb26fbc200e58c
mosaro404@gmail.com <mosaro404@gmail.com>
2025-05-15 20:59:18 +0200

    final

--- app/work/page.js
+++ app/work/page.js
@@ -17,8 +17,8 @@ const Work = () => {
         className="text-center mb-10 scroll-smooth"
         style={{ marginLeft: '30rem', marginRight: '30rem' }}
       >
-        <h2 className={`text-[30px] ${ovo.className}`}>My work</h2>
-        <p className={`text-[30px] ${ovo.className}`}>
+        <h2 className={`sm:text[18px] md:text[18px] lg:text-[30px] font-bold ${ovo.className}`}>My work</h2>
+        <p className={`sm:text[18px] md:text[18px] lg:text-[30px] ${ovo.className}`}>
           I have worked on several projects, including personal and collaborative ones.
           My portfolio showcases my skills in front-end development, with a focus on creating
           responsive and user-friendly interfaces. I am always eager to learn and take on new challenges.
