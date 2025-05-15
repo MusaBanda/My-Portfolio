b6a9a72b5592f5e2c8fabee786d27b9199e4032f
mosaro404@gmail.com <mosaro404@gmail.com>
2025-05-15 18:34:43 +0200

    final

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -4,12 +4,12 @@ import assets from '@/assets/assets'
 
 function footer() {
   return (
-    <div className=' mt-20 bg-[#ffe6e6] fill w-full h-[40rem]'>
+    <div className=' mt-20 bg-[#ffe6e6] fill w-full h-[25rem]'>
         <div className='text-center'>
-            < Image src={assets.logo_white}  alt='' className='w-16 mx-auto mb-2' style={{marginTop:'2rem'}}/>
+            < Image src={assets.logo_white} height={100} width={600} alt='' className='w-16 mx-auto mb-2' style={{marginTop:'2rem'}}/>
 
             <div className=' sm:text-[18px] md:text-[20px] lg:text-[30px]  w-max flex items-center gap-2 mx-auto mb-2
-            'style={{marginTop:'3rem', marginBottom:'0rem'}}>
+            'style={{marginTop:'2rem', marginBottom:'1rem'}}>
             
                   < Image src={assets.mail_icon} alt='' className='w-6' />
                   mosa.banda@hotmail.com
@@ -19,7 +19,7 @@ function footer() {
         mx-[10%] mt-12 py-6'>
             <p style={{marginTop:'5rem'}}>© 2025 Mthimkhulu Musa. All rights reserved.</p>
             <ul className='list-none sm:text-[18px] md:text-[20px] lg:text-[30px] '
-            style={{ display: 'flex', gap: '2rem', marginLeft:'-5rem',  marginRight: '1rem', marginBottom:'5rem'}}>
+            style={{ display: 'flex', gap: '2rem', marginLeft:'-5rem',  marginRight: '0.5rem', marginBottom:'0rem'}}>
                 <li ><a target='_blank' href="https://github.com/MusaBanda">Github</a></li>
                 <li ><a target='_blank' href="https://instagram.com/@musa_banda_kamthimkhulu/">Instagram</a></li>
                 <li ><a target='_blank' href="https://facebook.com/@mosa.banda.9/">Facebook</a></li>
