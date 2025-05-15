9f719fe9755548e7dd85dcffd41100534cb33221
mosaro404@gmail.com <mosaro404@gmail.com>
2025-05-15 17:33:56 +0200

    i changed all the re to px

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { assets } from '@/assets/assets';
 import Image from 'next/image';
-import React, { useEffect, useRef, userState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { ovo, roboto } from '../layout';
 
 const Navbar = () => {
@@ -9,22 +9,21 @@ const Navbar = () => {
 
     const sideMenuRref = useRef();
     const openMenu = () => {
-        sideMenuRref.current.style.transform = 'translateX(-16rem)';
+        sideMenuRref.current.style.transform = 'translateX(-256px)';  // -16rem = 256px
     }
     const closeMenu = () => {
-        sideMenuRref.current.style.transform = 'translateX(16rem)';
+        sideMenuRref.current.style.transform = 'translateX(256px)';   // 16rem = 256px
     }
 
     useEffect(() => {
         window.addEventListener('scroll', () => {
             if (scrollY > 50) {
                 setIsScroll(true);
-            }else{
+            } else {
                 setIsScroll(false);
             }
         })
-    },[])
-
+    }, [])
 
     return (
         <>
@@ -33,33 +32,33 @@ const Navbar = () => {
             </div>
             <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50  
                 ${isScroll ? "bg-[#ffe6e6] bg-opacity-50 backgrop-blur-lg shadow-sm" : ""} `}>    
-                <h1  className={`sm:text-[30px] md:text-[60px] lg:text-[120px] font-bold ${roboto.className}`}
-                style={{ padding: '1rem', color: 'black', marginLeft:'5rem',  marginRight: '0rem', marginTop:'3rem', marginBottom:'0rem'}}>
+                <h1 className={`sm:text-[30px] md:text-[60px] lg:text-[120px] font-bold ${roboto.className}`}
+                style={{ padding: '16px', color: 'black', marginLeft:'80px', marginRight: '0px', marginTop:'48px', marginBottom:'0px' }}>
                 MusaBanda<span style={{ color: 'red' }}>.</span>
                 </h1>
                 <ul className='absolute top-4 left-1/2 transform -translate-x-1/2 flex gap-6 list-none bg-white px-12 py-3 rounded-full 
                 shadow-sm bg-opacity-80 border-2 border-gray-400' 
-                style={{ padding: '2rem', border:'1px solid white',backgroundColor: 'white', marginLeft:'0rem',  marginRight: '1rem', marginTop:'3rem', marginBottom:'0rem'}} >
-                <li><a className={`sm:text-[18px] md:text-[20px] lg:text-[30px] no-underline ${ovo.className}`} style={{ padding: '1rem',color: 'black',}}  href='#top'>Home</a></li>
-                <li><a className={`sm:text-[18px] md:text-[20px] lg:text-[30px] no-underline ${ovo.className}`} style={{ padding: '1rem',color: 'black',}}  href='about'>About Me</a></li>
-                <li><a className={`sm:text-[18px] md:text-[20px] lg:text-[30px] no-underline ${ovo.className}`} style={{ padding: '1rem',color: 'black',}}  href='#services'>Services</a></li>
-                <li><a className={`sm:text-[18px] md:text-[20px] lg:text-[30px] no-underline ${ovo.className}`} style={{ padding: '1rem',color: 'black',}}  href='work'>My Work</a></li>
-                <li><a className={`sm:text-[18px] md:text-[20px] lg:text-[30px] no-underline ${ovo.className}`} style={{ padding: '1rem',color: 'black',}}  href='contact'>Contact Me</a></li>
+                style={{ padding: '32px', border:'1px solid white', backgroundColor: 'white', marginLeft:'0px', marginRight: '16px', marginTop:'48px', marginBottom:'0px' }} >
+                <li><a className={`sm:text-[18px] md:text-[20px] lg:text-[30px] no-underline ${ovo.className}`} style={{ padding: '16px',color: 'black' }}  href='#top'>Home</a></li>
+                <li><a className={`sm:text-[18px] md:text-[20px] lg:text-[30px] no-underline ${ovo.className}`} style={{ padding: '16px',color: 'black' }}  href='about'>About Me</a></li>
+                <li><a className={`sm:text-[18px] md:text-[20px] lg:text-[30px] no-underline ${ovo.className}`} style={{ padding: '16px',color: 'black' }}  href='#services'>Services</a></li>
+                <li><a className={`sm:text-[18px] md:text-[20px] lg:text-[30px] no-underline ${ovo.className}`} style={{ padding: '16px',color: 'black' }}  href='work'>My Work</a></li>
+                <li><a className={`sm:text-[18px] md:text-[20px] lg:text-[30px] no-underline ${ovo.className}`} style={{ padding: '16px',color: 'black' }}  href='contact'>Contact Me</a></li>
                 </ul>
                 <div className='flex items-center gap-4'>
 
                    <Image src={assets.moon_icon} alt='moon_icon' width={40} height={40} 
                     className='cursor-pointer mr-14'
-                    style={{ marginLeft:'5rem',  marginRight: '1rem',  marginTop:'3rem',}}  />
+                    style={{ marginLeft:'80px', marginRight: '16px', marginTop:'48px' }}  />
 
                 <a href='contact' className={`sm:text-[18px] md:text-[20px] lg:text-[30px] no-underline bg-[white] ${ovo.className}`}  
                 style={{
-                padding: '2rem', color: 'black', marginTop:'3rem',
-                border: '2px solid #ccc', borderRadius: '2rem',
-                marginLeft: '1rem', marginRight: '8rem',
+                padding: '32px', color: 'black', marginTop:'48px',
+                border: '2px solid #ccc', borderRadius: '32px',
+                marginLeft: '16px', marginRight: '128px',
                 }} 
                 >CONTACT<Image src={assets.arrow_icon}
-                    alt='' width={15} height={15} style={{ marginLeft:'2rem',  marginTop: '0rem',}}/> </a>
+                    alt='' width={15} height={15} style={{ marginLeft:'32px', marginTop: '0px' }}/> </a>
 
                     <a href='' className='hidden block md:hidden ml-3' onClick={openMenu}><Image src={assets.menu_black} 
                     alt='' width={40} height={40} /></a>
